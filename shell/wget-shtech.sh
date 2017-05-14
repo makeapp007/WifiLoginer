721eb64a0175ec7a14f78a804fe80f66998e6cca
makeapp <liuyh1@shanghaitech.edu.cn>
2017-05-14 20:42:51 +0800

    Add script for wifi-login

--- shell/wget-shtech.sh
+++ shell/wget-shtech.sh
@@ -1,1 +1,2 @@
 #!/bin/sh
+wget --header="Content-Type: application/x-www-form-urlencoded" --header="Cookie:JSESSIONID=D56359E00B58C7877668AAB44B3BFE31" --post-data="userName=username&password=password&hasValidateCode=false&authLan=zh_CN" -O - -q https://controller1.net.shanghaitech.edu.cn:8445/PortalServer//Webauth/webAuthAction\!login.action 
